24fbc8ba4987d2ac79bdc1bf7cb8632e33ff0079
snehajgec <sneha.jgec@gmail.com>
2020-04-04 17:19:15 +0530

    Word Problem 2 Recursive Leetcode

--- src/DynamicProgramming/WordBreakProblem2.java
+++ src/DynamicProgramming/WordBreakProblem2.java
@@ -3,40 +3,57 @@ package DynamicProgramming;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 public class WordBreakProblem2 {
 
 	public static void main(String[] args) {
 
 		List<String> dictionary = new ArrayList<String>();
 
-		String temp_dictionary[] = { "he", "hell", "hello", "I", "there", "the", "hope", "you", "are", "are", "having",
-				"a", "nice", "day" };
+		/*
+		 * String temp_dictionary[] = { "he", "hell", "hello", "I", "there", "the",
+		 * "hope", "you", "are", "are", "having", "a", "nice", "day" };
+		 */
+		String temp_dictionary[] = { "cats","cat", "and", "sand", "dog"};
 
+		
 		for (String st : temp_dictionary) {
 			dictionary.add(st);
 		}
 
-		String word = "hellothereIhopeyouarehavinganiceday";
+		//String word = "hellothereIhopeyouarehavinganiceday";
+		String word = "catsanddog";
 
 		wordBreak(word, dictionary);
 //        System.out.println(wordBreak("catsandog", dictionary)); 
 
 	}
 
-	public static void wordBreak(String s, List<String> wordDict) {
+	public static List<String> wordBreak(String s, List<String> wordDict) {
 		String out = "";
-		wordBreakPrint(s, wordDict, out);
+		List<String> ans = new ArrayList<>();
+		wordBreakPrint(s, wordDict, out, ans);
+		return ans;
 	}
 
-	public static void wordBreakPrint(String s, List<String> wordDict, String out) {
+	public static void wordBreakPrint(String s, List<String> wordDict, String out, List<String> ans) {
 		if (s.length() == 0) {
+			ans.add(out);
 			System.out.println(out);
 			return;
 		}
 		for (int i = 1; i <= s.length(); i++) {
 			String prefix = s.substring(0, i);
 			if (wordDict.contains(s.substring(0, i))) {
-				wordBreakPrint(s.substring(i, s.length()), wordDict, out + " " + prefix);
+				String output;
+				if(out.equals("")) {
+					output = out + prefix;
+				}
+				else {
+					output = out + " " + prefix;
+				}
+				wordBreakPrint(s.substring(i, s.length()), wordDict, output, ans);
 
 			}
 		}
---
+++ src/MoveZeroes.java
@@ -0,0 +1,9 @@
+
+public class MoveZeroes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
